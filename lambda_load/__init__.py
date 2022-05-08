35cb360c4f1b187aaa2945d55596ab7177763ff1
Matheus Couto <matheusccouto@gmail.com>
2022-05-07 22:26:26 -0300

    Update load to work with bigquery

--- lambda_load/__init__.py
+++ lambda_load/__init__.py
@@ -1,70 +1,84 @@
 """Load players data from Cartola FC to the database."""
 
-import csv
 import io
 import os
 
 import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
 import utils.aws.s3
 import utils.cockroachdb
 
-engine = utils.cockroachdb.create_engine(
-    user=os.environ["USER"],
-    password=os.environ["PASSWORD"],
-    host=os.environ["HOST"],
-    port=os.environ["PORT"],
-    database=os.environ["DATABASE"],
-    options=os.environ["OPTIONS"],
+LOCATION = "us-east4"
+
+creds = service_account.Credentials.from_service_account_info(
+    info={
+        "type": os.environ["GCP_TYPE"],
+        "project_id": os.environ["GCP_PROJECT_ID"],
+        "private_key_id": os.environ["GCP_PRIVATE_KEY_ID"],
+        "private_key": os.environ["GCP_PRIVATE_KEY"].replace("\\n", "\n"),
+        "client_email": os.environ["GCP_CLIENT_EMAIL"],
+        "client_id": os.environ["GCP_CLIENT_ID"],
+        "auth_uri": os.environ["GCP_AUTH_URI"],
+        "token_uri": os.environ["GCP_TOKEN_URI"],
+        "auth_provider_x509_cert_url": os.environ["GCP_AUTH_PROVIDER_X509_CERT_URL"],
+        "client_x509_cert_url": os.environ["GCP_CLIENT_X509_CERT_URL"],
+    }
+)
+client = bigquery.Client(
+    project=creds.project_id, location="us-east4", credentials=creds
 )
 
 
-def create_update_query(table, cols, vals, subset):
+def create_match_query(table, cols, vals, subset):
     """Create update statement."""
     set_ = ", ".join([f'"{c}" = {v}' for c, v in zip(cols, vals)])
     where = " AND ".join([f'"{c}" = {v}' for c, v in zip(cols, vals) if c in subset])
     return f"UPDATE {table} SET {set_} WHERE {where}"
 
 
-def create_insert_query(table, cols, vals):
+def create_match_query(table, cols, vals, subset):
+    """Create update statement."""
+    set_ = ", ".join([f'"{c}" = {v}' for c, v in zip(cols, vals)])
+    where = " AND ".join([f'"{c}" = {v}' for c, v in zip(cols, vals) if c in subset])
+    return f"UPDATE {table} SET {set_} WHERE {where}"
+
+
+def create_insert_query(cols):
     """Create insert statement."""
-    cols = ", ".join([f'"{c}"' for c in cols])
-    vals = ", ".join([str(v) for v in vals])
-    return f"INSERT INTO {table} ({cols}) VALUES ({vals})"
+    cols = ", ".join([f"{c}" for c in cols])
+    return f"INSERT ({cols}) VALUES ({cols})"
 
 
 def handler(event, context=None):
     """Lambda handler."""
-    data = pd.read_csv(io.StringIO(utils.aws.s3.load(event["uri"])), index_col=0)
+    data = pd.read_csv(io.StringIO(utils.aws.s3.load(event["uri"])), index_col=0) + 6
     table = event["schema"] + "." + event["table"]
+    tmp_table = event["schema"] + ".tmp_" + event["table"]
 
-    update_count = 0
-    insert_count = 0
-    with engine.connect() as conn:
-        for _, row in data.iterrows():
-            values = (
-                row.to_csv(
-                    header=False,
-                    index=False,
-                    quoting=csv.QUOTE_NONNUMERIC,
-                    quotechar="'",
-                    na_rep="NULL",
-                )
-                .replace("'NULL'", "NULL")
-                .strip()
-                .split("\n")
-            )
+    data.to_gbq(
+        destination_table=tmp_table,
+        project_id=os.environ["GCP_PROJECT_ID"],
+        if_exists="replace",
+        credentials=creds,
+    )
+    cols = ",".join(data.columns)
+    match = " AND ".join([f"a.{col} = b.{col}" for col in event["subset"]])
+    update = ", ".join([f"a.{col} = b.{col}" for col in data.columns])
 
-            query = create_update_query(table, row.index, values, event["subset"])
-            res = conn.execute(query)
-            update_count += res.rowcount
-            if res.rowcount == 0:  # No match
-                query = create_insert_query(table, row.index, values)
-                conn.execute(query)
-                insert_count += 0
+    query = f"""
+    MERGE {table} a
+    USING {tmp_table} b
+        ON {match}
+    WHEN MATCHED THEN
+        UPDATE SET {update}
+    WHEN NOT MATCHED THEN
+        INSERT ({cols}) VALUES ({cols})
+    """
+    job = client.query(query, location=LOCATION, project=os.environ["GCP_PROJECT_ID"])
 
     return {
         "statusCode": 200,
-        "updated": update_count,
-        "inserted": insert_count,
+        # "message": job.result()._total_rows,
     }
