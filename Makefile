95bbb0556f96e8536926c9c84c07f93816f9965f
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-05-09 21:40:35 -0300

    Dev (#15)
    Migrate do BigQuery and implement DBT

--- Makefile
+++ Makefile
@@ -6,21 +6,21 @@ deploy-dev:
 	zip -r layer_pandas.zip python/
 	rm -r python
 	mkdir python
-	pip install -r requirements-cockroachdb.txt --target python
-	zip -r layer_cockroachdb.zip python/
+	pip install -r requirements-bigquery.txt --target python
+	zip -r layer_bigquery.zip python/
 	rm -r python
 	sls deploy --stage dev --aws-profile palpiteiro
 	rm layer_pandas.zip
-	rm layer_cockroachdb.zip
+	rm layer_bigquery.zip
 deploy-main:
 	mkdir python
 	pip install -r requirements-pandas.txt --target python
 	zip -r layer_pandas.zip python/
 	rm -r python
 	mkdir python
-	pip install -r requirements-cockroachdb.txt --target python
-	zip -r layer_cockroachdb.zip python/
+	pip install -r requirements-bigquery.txt --target python
+	zip -r layer_bigquery.zip python/
 	rm -r python
 	sls deploy --stage main --aws-profile palpiteiro
 	rm layer_pandas.zip
-	rm layer_cockroachdb.zip
+	rm layer_bigquery.zip
--- lambda_load/__init__.py
+++ lambda_load/__init__.py
@@ -1,70 +1,81 @@
 """Load players data from Cartola FC to the database."""
 
-import csv
 import io
+import json
 import os
+import time
 
 import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
 import utils.aws.s3
-import utils.cockroachdb
 
-engine = utils.cockroachdb.create_engine(
-    user=os.environ["USER"],
-    password=os.environ["PASSWORD"],
-    host=os.environ["HOST"],
-    port=os.environ["PORT"],
-    database=os.environ["DATABASE"],
-    options=os.environ["OPTIONS"],
+LOCATION = "us-east4"
+
+creds = service_account.Credentials.from_service_account_info(
+    info={
+        "type": os.environ["GCP_SERVICE_ACCOUNT_TYPE"],
+        "project_id": os.environ["GCP_SERVICE_ACCOUNT_PROJECT_ID"],
+        "private_key_id": os.environ["GCP_SERVICE_ACCOUNT_PRIVATE_KEY_ID"],
+        "private_key": os.environ["GCP_SERVICE_ACCOUNT_PRIVATE_KEY"].replace(
+            "\\n", "\n"
+        ),
+        "client_email": os.environ["GCP_SERVICE_ACCOUNT_CLIENT_EMAIL"],
+        "client_id": os.environ["GCP_SERVICE_ACCOUNT_CLIENT_ID"],
+        "auth_uri": os.environ["GCP_SERVICE_ACCOUNT_AUTH_URI"],
+        "token_uri": os.environ["GCP_SERVICE_ACCOUNT_TOKEN_URI"],
+        "auth_provider_x509_cert_url": os.environ[
+            "GCP_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL"
+        ],
+        "client_x509_cert_url": os.environ["GCP_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL"],
+    }
 )
-
-
-def create_update_query(table, cols, vals, subset):
-    """Create update statement."""
-    set_ = ", ".join([f'"{c}" = {v}' for c, v in zip(cols, vals)])
-    where = " AND ".join([f'"{c}" = {v}' for c, v in zip(cols, vals) if c in subset])
-    return f"UPDATE {table} SET {set_} WHERE {where}"
-
-
-def create_insert_query(table, cols, vals):
-    """Create insert statement."""
-    cols = ", ".join([f'"{c}"' for c in cols])
-    vals = ", ".join([str(v) for v in vals])
-    return f"INSERT INTO {table} ({cols}) VALUES ({vals})"
+client = bigquery.Client(
+    project=creds.project_id,
+    location=LOCATION,
+    credentials=creds,
+)
 
 
 def handler(event, context=None):
     """Lambda handler."""
-    data = pd.read_csv(io.StringIO(utils.aws.s3.load(event["uri"])), index_col=0)
     table = event["schema"] + "." + event["table"]
+    tmp_table = event["schema"] + ".tmp_" + event["table"]
 
-    update_count = 0
-    insert_count = 0
-    with engine.connect() as conn:
-        for _, row in data.iterrows():
-            values = (
-                row.to_csv(
-                    header=False,
-                    index=False,
-                    quoting=csv.QUOTE_NONNUMERIC,
-                    quotechar="'",
-                    na_rep="NULL",
-                )
-                .replace("'NULL'", "NULL")
-                .strip()
-                .split("\n")
-            )
+    file = utils.aws.s3.load(event["uri"])
+    data = pd.read_csv(io.StringIO(file), index_col=0).convert_dtypes()
+    data.to_gbq(
+        destination_table=tmp_table,
+        project_id=creds.project_id,
+        if_exists="replace",
+        credentials=creds,
+        location=LOCATION,
+    )
 
-            query = create_update_query(table, row.index, values, event["subset"])
-            res = conn.execute(query)
-            update_count += res.rowcount
-            if res.rowcount == 0:  # No match
-                query = create_insert_query(table, row.index, values)
-                conn.execute(query)
-                insert_count += 0
+    cols = ",".join(data.columns)
+    match = " AND ".join([f"a.{col} = b.{col}" for col in event["subset"]])
+    update = ", ".join([f"a.{col} = b.{col}" for col in data.columns])
 
+    query = f"""
+    MERGE {table} a
+    USING {tmp_table} b
+        ON {match}
+    WHEN MATCHED THEN
+        UPDATE SET {update}
+    WHEN NOT MATCHED THEN
+        INSERT ({cols}) VALUES ({cols})
+    """
+    job = client.query(query, location=LOCATION, project=creds.project_id)
+
+    while not job.done():
+        time.sleep(0.1)
+
+    if job.errors:
+        raise RuntimeError(json.loads(job.errors))
+
+    client.delete_table(tmp_table)
     return {
         "statusCode": 200,
-        "updated": update_count,
-        "inserted": insert_count,
+        "num_affected_rows": job.num_dml_affected_rows,
     }
