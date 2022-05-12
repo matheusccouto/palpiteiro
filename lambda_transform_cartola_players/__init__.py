42ef14ceab0e0d738e253a25e2347aecf27b8783
Matheus Couto <matheusccouto@gmail.com>
2022-05-12 20:06:29 -0300

    test lambda_transform_cartola_matches

--- lambda_transform_cartola_players/__init__.py
+++ lambda_transform_cartola_players/__init__.py
@@ -9,11 +9,11 @@ import utils.aws.s3
 import utils.http
 
 
-def handler(event, context=None):
+def handler(event, context=None):  # pylint: disable=unused-argument
     """Lambda handler."""
     # Load
     data = json.loads(utils.aws.s3.load(event["uri"]))
-    
+
     # Transform
     data = data["atletas"]
     for row in data:
