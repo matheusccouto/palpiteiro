37f6c799cd79ee33e875b284b0e169931907cdd8
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-08-10 12:24:54 +0000

    Update model

--- function_points_train/query.sql
+++ function_points_train/query.sql
@@ -27,7 +27,7 @@ SELECT
     received_penalties_club_last_5,
     received_penalties_opponent_last_5,
     played_last_5,
-    IF(total_points < 0, 0, total_points) AS total_points
+    IF(total_points < 0.01, 0.01, total_points) AS total_points
 FROM
     palpiteiro.fct_player
 WHERE
