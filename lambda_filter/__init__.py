d56e118a8150a3edec8ec918b78c012c2916c213
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-08-20 17:57:49 +0000

    Fix schedule

--- lambda_filter/__init__.py
+++ lambda_filter/__init__.py
@@ -5,24 +5,20 @@ from zoneinfo import ZoneInfo
 
 TZ = ZoneInfo("America/Sao_Paulo")
 
+
 def handler(event, context=None):  # pylint: disable=unused-argument
     """Lambda handler."""
     players = event["players"]
     date = event.get("date")
     if date is not None:
 
-        selected_timestamp = datetime.strptime(date, '%Y-%m-%d').replace(tzinfo=TZ)
+        selected_timestamp = datetime.strptime(date, "%Y-%m-%d").replace(tzinfo=TZ)
         selected_date = selected_timestamp.date()
-        # print(selected_date)
-
-        print(datetime.fromisoformat(event["players"][0]["timestamp"]).astimezone(TZ).date())
-
         event["players"] = [
             p
             for p in players
-            if datetime.fromisoformat(p["timestamp"])
-            .astimezone(TZ)
-            .date()
+            if datetime.fromisoformat(p["timestamp"]).astimezone(TZ).date()
             == selected_date
         ]
+
     return event
