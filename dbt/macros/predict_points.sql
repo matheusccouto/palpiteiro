d83c41a5f5943a54452ad0cd8270f4dda930ce53
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-09-07 20:06:38 -0300

    Update predict_points.sql

--- dbt/macros/predict_points.sql
+++ dbt/macros/predict_points.sql
@@ -26,7 +26,7 @@ CREATE OR REPLACE FUNCTION {{ target.dataset }}.predict_points(
     avg_odds_draw FLOAT64
 ) RETURNS FLOAT64 REMOTE WITH CONNECTION `us-east4.remote-function` OPTIONS (
     endpoint = 'https://us-east4-palpiteiro-{{ target.name }}.cloudfunctions.net/points',
-    user_defined_context = [("mode", "position,total_points_last_5_at,offensive_points_last_5_at,defensive_points_last_5_at,spi_club,spi_opponent,prob_club,prob_opponent,prob_tie,importance_club,importance_opponent,proj_score_club,proj_score_opponent,total_points_club_last_5_at,offensive_points_club_last_5_at,defensive_points_club_last_5_at,total_allowed_points_opponent_last_5_at,offensive_allowed_points_opponent_last_5_at,defensive_allowed_points_opponent_last_5_at,played_last_5,avg_odds_club,avg_odds_opponent,avg_odds_draw")]
+    user_defined_context = [("names", "position,total_points_last_5_at,offensive_points_last_5_at,defensive_points_last_5_at,spi_club,spi_opponent,prob_club,prob_opponent,prob_tie,importance_club,importance_opponent,proj_score_club,proj_score_opponent,total_points_club_last_5_at,offensive_points_club_last_5_at,defensive_points_club_last_5_at,total_allowed_points_opponent_last_5_at,offensive_allowed_points_opponent_last_5_at,defensive_allowed_points_opponent_last_5_at,played_last_5,avg_odds_club,avg_odds_opponent,avg_odds_draw")]
 )
 
 {% endmacro %}
